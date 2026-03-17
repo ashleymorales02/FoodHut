83aaa1426375d5a716357817f2fa42fe710e0c21
Ashley <moralesashley16@gmail.com>
2026-03-17 01:04:38 -0400

    Add files via upload

---
+++ newProjectTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  newProjectTestApp.swift
+//  newProjectTest
+//
+//  Created by Ashley Morales on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct newProjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
